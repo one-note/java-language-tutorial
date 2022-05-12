b8d387d46e9400c0a9b279fb13b07109c8877cc6
r0p06np <rabindra.kumar.patra@walmart.com>
2022-05-12 12:31:37 +0530

    added binary string for int types

--- src/com/java/basics/datatypes/PrimitiveMain.java
+++ src/com/java/basics/datatypes/PrimitiveMain.java
@@ -10,9 +10,17 @@ public class PrimitiveMain {
         // and right side of k is assigned value. assigned value to k is 10.
 
         // https://www.w3schools.com/java/java_data_types.asp
+        short s = 23;
         char c = 'a'; // only one character can be stored
+        System.out.println((int)c); // ascii = 97
+        System.out.println(Integer.toBinaryString(c)); // binary representation
+
         int x = 1000000000;
+        System.out.println(Integer.toBinaryString(x));
+
         long l = 10000000000l; // we can't store this value with a int variable
+        System.out.println(Long.toBinaryString(l));
+
         float f = 2.34f; // read yourself: https://www.geeksforgeeks.org/difference-between-single-precision-and-double-precision/
         double d = 2.34d; // read yourself: https://www.geeksforgeeks.org/introduction-of-floating-point-representation/
         boolean bool = false; // true or false can be stored
