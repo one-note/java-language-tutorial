84024107bfa92b52eb0f8bd85159808e93776a63
r0p06np <rabindra.kumar.patra@walmart.com>
2022-07-26 20:46:03 +0530

    array list examples

--- src/com/java/basics/collections/ArrayListMain.java
+++ src/com/java/basics/collections/ArrayListMain.java
@@ -1,14 +1,20 @@
 package com.java.basics.collections;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
+
+// add
+// remove
+// get
+// update
+// contains
 // size can be increased dynamically
 public class ArrayListMain {
 
 
-    private static class Customer {
+    private static class Customer implements Comparable<Customer> {
         String name;
+        String email;
 
         public Customer(String name) {
             this.name = name;
@@ -17,9 +23,174 @@ public class ArrayListMain {
         public String getName() {
             return name;
         }
+
+        //        @Override
+        public boolean equals(Object secondObject) {
+            // if this equals method is absent the object class equals method will be called which compares reference
+            // and not the content of object. for example : name , email
+            Customer c = (Customer) secondObject;
+            return this.name.equals(c.name);
+        }
+
+        @Override
+        public String toString() {
+            return "Customer{" +
+                    "name='" + name + '\'' +
+                    '}';
+        }
+
+        @Override
+        public int compareTo(Customer o) {
+            return -this.name.compareTo(o.name);
+        }
+    }
+
+
+    private static class CustomerNameComparator implements Comparator<Customer> {
+        @Override
+        public int compare(Customer o1, Customer o2) {
+            return -o1.name.compareTo(o2.name);
+        }
     }
 
     public static void main(String[] args) {
+        Customer c1 = new Customer("John");
+        Customer c2 = new Customer("Alex");
+        Customer c3 = new Customer("Alistair");
+        List<Customer> customers = new ArrayList<>();
+        customers.add(c1);
+        customers.add(c2);
+        customers.add(c3);
+        System.out.println(customers);
+        Collections.sort(customers, new CustomerNameComparator());
+        System.out.println(customers);
+    }
+
+    public static void m13(String[] args) {
+        Customer c1 = new Customer("John");
+        Customer c2 = new Customer("Alex");
+        Customer c3 = new Customer("Alistair");
+        List<Customer> customers = new ArrayList<>();
+        customers.add(c1);
+        customers.add(c2);
+        customers.add(c3);
+        System.out.println(customers);
+        Collections.sort(customers);
+        System.out.println(customers);
+    }
+
+    public static void m12(String[] args) {
+        List<String> names = new ArrayList<>();
+        names.add("John");
+        names.add("Alex");
+        Collections.sort(names);
+        Collections.reverse(names);
+        System.out.println(names);
+    }
+
+    public static void m11(String[] args) {
+        List<String> names = new ArrayList<>();
+        names.add("John");
+        names.add("Alex");
+        List<String> unmodifiableList = Collections.unmodifiableList(names);
+        names.add("Joe");
+
+        System.out.println(names);
+        System.out.println(unmodifiableList);
+        unmodifiableList.add("alex1");
+    }
+
+    public static void m10(String[] args) {
+        final int x = 10;
+//        x = 11;
+
+        final List<String> names = new ArrayList<>();
+        names.add("John");
+        names.add("Alex");
+        System.out.println(names);
+
+    }
+
+    public static void m9(String[] args) {
+        Customer c = new Customer("Alex");
+        Object o = c;
+        Object o1 = new Customer("John");
+        System.out.println(o1.getClass());
+        System.out.println(o.getClass());
+        List<String> names = new ArrayList<>();
+
+
+        names.add("John");
+        names.add("Alex");
+
+        System.out.println(names);
+    }
+
+    public static void m8(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+        names.add("John");
+        names.add("Alex");
+
+        System.out.println(names
+                .isEmpty());
+        names.clear();
+        System.out.println(names);
+        System.out.println(names.isEmpty());
+    }
+
+    public static void m7(String[] args) {
+
+        ArrayList<String> names = new ArrayList<>();
+        names.add("John");
+        names.add("Alex");
+
+        System.out.println(names);
+
+        names.set(1, "Joe");
+
+        System.out.println(names);
+
+        names.set(2, "Joe"); // Exception in thread "main" java.lang.IndexOutOfBoundsException: Index: 2, Size: 2
+
+    }
+
+    public static void m6(String[] args) {
+        Customer c1 = new Customer("John");
+        Customer c2 = new Customer("Alex");
+
+        ArrayList<Customer> customerList1 = new ArrayList<>();
+        customerList1.add(c1);
+        customerList1.add(c1);
+        ArrayList<Customer> customerList2 = new ArrayList<>();
+        customerList2.add(c2);
+        customerList2.addAll(customerList1);
+
+        System.out.println(customerList1);
+        System.out.println(customerList2);
+
+        customerList2.retainAll(customerList1); // internally uses equals method
+        System.out.println(customerList2);
+    }
+
+    public static void m5(String[] args) {
+        Customer c1 = new Customer("John");
+        Customer c2 = new Customer("Alex");
+
+        ArrayList<Customer> customerList1 = new ArrayList<>();
+        customerList1.add(c1);
+        customerList1.add(c1);
+        ArrayList<Customer> customerList2 = new ArrayList<>();
+        customerList2.add(c2);
+        customerList2.addAll(customerList1);
+
+        System.out.println(customerList1);
+        System.out.println(customerList2);
+
+        customerList2.removeAll(customerList1); // internally uses equals method
+        System.out.println(customerList2);
+    }
+
+    public static void m4(String[] args) {
         Customer c1 = new Customer("John");
         Customer c2 = new Customer("Alex");
 
@@ -32,15 +203,17 @@ public class ArrayListMain {
 
         Customer c3 = new Customer("John");
         System.out.println(customers.contains(c3));
+        Customer c4 = new Customer("John");
+        System.out.println(customers.remove(c4));
+
 
         String s = "John";
         String s1 = "John";
         ArrayList<String> list = new ArrayList<>();
-        list
-                .add(s);
+        list.add(s);
 
         System.out.println(list
-                .contains(s1));
+                .contains(s1)); // equals method of string class
 
     }
 
